78ea921da17af68a5933d35388d26f811a41b928
Suresh Rajasekar <suresh.rajasekar89@gmail.com>
2024-01-15 15:53:48 +0530

    suresh commit

--- script.py
+++ script.py
@@ -4,8 +4,8 @@ print("script 1 from main changed LINE 3")#Old Code
 print("script 1 from main changed LINE 4")#Old Code
 print("script 1 from main changed LINE 5")#Old Code
 
-print("script 1 from main changed LINE 6")#Old Code
-print("script 1 from main changed LINE 7")#Old Code
+print("script 1 from main changed LINE 6")#new Code
+print("script 1 from main changed LINE 7")#new Code
 print("script 1 from main changed LINE 8")#Old Code
 print("script 1 from main changed LINE 9")#Old Code
 print("script 1 from main changed LINE 10")#Old Code
