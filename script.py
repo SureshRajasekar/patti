bf0e7400b4e0fd162894adff1435d0cae07ca9ac
Suresh Rajasekar <suresh.rajasekar89@gmail.com>
2024-01-15 18:09:49 +0530

    Added mul func in patti2

--- script.py
+++ script.py
@@ -9,3 +9,4 @@ print("script 1 from main changed LINE 7")#new Code
 print("script 1 from main changed LINE 8")#Old Code
 print("script 1 from main changed LINE 9")#Old Code
 print("script 1 from main changed LINE 10")#Old Code
+int mul(int x, int y) ( return x*y; }
