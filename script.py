066f8bcad52e69f4c69e9833c573a2e948bc5a42
SureshRajasekar <59593428+SureshRajasekar@users.noreply.github.com>
2024-01-15 15:40:03 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,11 @@
+print("script 1 from main changed LINE 1")#Old Code
+print("script 1 from main changed LINE 2")#Old Code
+print("script 1 from main changed LINE 3")#Old Code
+print("script 1 from main changed LINE 4")#Old Code
+print("script 1 from main changed LINE 5")#Old Code
+
+print("script 1 from main changed LINE 6")#Old Code
+print("script 1 from main changed LINE 7")#Old Code
+print("script 1 from main changed LINE 8")#Old Code
+print("script 1 from main changed LINE 9")#Old Code
+print("script 1 from main changed LINE 10")#Old Code
