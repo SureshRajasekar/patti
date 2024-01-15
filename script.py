da142e653662150dd0a2d1d22249b0481f54ef1c
Suresh Rajasekar <suresh.rajasekar89@gmail.com>
2024-01-15 18:08:03 +0530

    Added sum func in patti branch

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ print("script 1 from main changed LINE 2")#new Code
 print("script 1 from main changed LINE 3")#new Code
 print("script 1 from main changed LINE 4")#Old Code
 print("script 1 from main changed LINE 5")#Old Code
+int sum(int a, int b){return a+b; }
 
 print("script 1 from main changed LINE 6")#new Code
 print("script 1 from main changed LINE 7")#new Code
